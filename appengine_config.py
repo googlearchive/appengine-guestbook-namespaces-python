03b423067f4a97b6425e6070cddc32944fd87bca
Daniel Hermes <dhermes@google.com>
2013-08-08 12:54:00 -0700

    Updating guestbook to the latest standard guestbook.
    Reviewed in https://codereview.appspot.com/12558045/

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python2.4
-#
 # Copyright 2010 Google Inc. All Rights Reserved.
 
 """
@@ -33,10 +31,12 @@ every query.
 
 __author__ = 'nverne@google.com (Nicholas Verne)'
 
+
 import Cookie
 import os
 
 from google.appengine.api import namespace_manager
+
 
 _USE_SERVER_NAME = 0
 _USE_GOOGLE_APPS_DOMAIN = 1
@@ -44,31 +44,30 @@ _USE_COOKIE = 2
 
 _NAMESPACE_PICKER = _USE_SERVER_NAME
 
+
 def namespace_manager_default_namespace_for_request():
-  """Determine which namespace is to be used for a request.
+    """Determine which namespace is to be used for a request.
 
-  The value of _NAMESPACE_PICKER has the following effects:
+    The value of _NAMESPACE_PICKER has the following effects:
 
-  If _USE_SERVER_NAME, we read server name
-  foo.guestbook-isv.appspot.com and set the namespace.
+    If _USE_SERVER_NAME, we read server name
+    foo.guestbook-isv.appspot.com and set the namespace.
 
-  If _USE_GOOGLE_APPS_DOMAIN, we allow the namespace manager to infer
-  the namespace from the request.
+    If _USE_GOOGLE_APPS_DOMAIN, we allow the namespace manager to infer
+    the namespace from the request.
 
-  If _USE_COOKIE, then the ISV might have a gateway page that sets a
-  cookie called 'namespace', and we set the namespace to the cookie's value
-  """
-  name = None
+    If _USE_COOKIE, then the ISV might have a gateway page that sets a
+    cookie called 'namespace', and we set the namespace to the cookie's value
+    """
+    name = None
 
-  if _NAMESPACE_PICKER == _USE_SERVER_NAME:
-    name = os.environ['SERVER_NAME']
-  elif _NAMESPACE_PICKER == _USE_GOOGLE_APPS_DOMAIN:
-    name = namespace_manager.google_apps_namespace()
-  elif _NAMESPACE_PICKER == _USE_COOKIE:
-    cookies = os.environ.get('HTTP_COOKIE', None)
-    if cookies:
-      name = Cookie.BaseCookie(cookies).get('namespace')
+    if _NAMESPACE_PICKER == _USE_SERVER_NAME:
+        name = os.environ['SERVER_NAME']
+    elif _NAMESPACE_PICKER == _USE_GOOGLE_APPS_DOMAIN:
+        name = namespace_manager.google_apps_namespace()
+    elif _NAMESPACE_PICKER == _USE_COOKIE:
+        cookies = os.getenv('HTTP_COOKIE', None)
+        if cookies:
+            name = Cookie.BaseCookie(cookies).get('namespace')
 
-  return name
-
-
+    return name
--- guestbook.py
+++ guestbook.py
@@ -1,65 +1,84 @@
-import cgi
-import datetime
-import wsgiref.handlers
+import os
+import urllib
 
-from google.appengine.ext import db
 from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import ndb
 
-class Greeting(db.Model):
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
+import jinja2
+import webapp2
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
-class MainPage(webapp.RequestHandler):
-  def get(self):
-    self.response.out.write('<html><body>')
-
-    greetings = db.GqlQuery("SELECT * "
-                            "FROM Greeting "
-                            "ORDER BY date DESC LIMIT 10")
-
-    for greeting in greetings:
-      if greeting.author:
-        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
-      else:
-        self.response.out.write('An anonymous person wrote:')
-      self.response.out.write('<blockquote>%s</blockquote>' %
-                              cgi.escape(greeting.content))
-
-    # Write the submission form and the footer of the page
-    self.response.out.write("""
-          <form action="/sign" method="post">
-            <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Sign Guestbook"></div>
-          </form>
-        </body>
-      </html>""")
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 
-class Guestbook(webapp.RequestHandler):
-  def post(self):
-    greeting = Greeting()
+# We set a parent key on the 'Greetings' to ensure that they are all in the same
+# entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
-
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/')
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
+    return ndb.Key('Guestbook', guestbook_name)
 
 
-application = webapp.WSGIApplication([
-  ('/', MainPage),
-  ('/sign', Guestbook)
+class Greeting(ndb.Model):
+    """Models an individual Guestbook entry with author, content, and date."""
+    author = ndb.UserProperty()
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
+class Guestbook(webapp2.RequestHandler):
+
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each Greeting
+        # is in the same entity group. Queries across the single entity group
+        # will be consistent. However, the write rate to a single entity group
+        # should be limited to ~1/second.
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        if users.get_current_user():
+            greeting.author = users.get_current_user()
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/sign', Guestbook),
 ], debug=True)
-
-
-def main():
-  run_wsgi_app(application)
-
-
-if __name__ == '__main__':
-  main()
